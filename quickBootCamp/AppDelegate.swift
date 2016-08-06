c1abef43ae6be9e1501720b7bde4dcb23e0025d3
Sang Hyeon <rlatkdgus500@gmail.com>
2016-08-06 09:09:40 +0900

    Initial Commit

---
+++ quickBootCamp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  quickBootCamp
+//
+//  Created by 김상현 on 2016. 8. 6..
+//  Copyright © 2016년 orcacode. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
